5a928a496ff48f9f15bf23d06edb3236df2818b7
devansh59 <113086973+devansh59@users.noreply.github.com>
2022-09-08 11:31:28 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,16 @@
+console.log("Welcome to node js");
+var a=5;
+var b=5;
+var c=a+b;
+console.log("Sum is = "+c);
+//Es6
+console.log(`A value is ${a} B value is ${b}`);
+
+if(c==10)
+{
+    console.log("It is correct")
+}
+else
+{
+    console.log("It is wrong")
+}
